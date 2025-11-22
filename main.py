8ed6e59f83358825dba14d582d2cd992bf3492d3
sumerashar <164212857+sumerashar@users.noreply.github.com>
2025-11-22 15:19:06 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+import pickle
+import streamlit as st
+import requests
+
+
+# ---------------------------
+# Helper functions
+# ---------------------------
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+    return recommended_movie_names, recommended_movie_posters
+
+# ---------------------------
+# Load data (files are in project root)
+# ---------------------------
+with open('movies.pkl', 'rb') as f:
+    movies = pickle.load(f)
+
+with open('similarity.pkl', 'rb') as f:
+    similarity = pickle.load(f)
+
+# ---------------------------
+# Streamlit UI
+# ---------------------------
+st.header('Movie Recommender System')
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    for col, name, poster in zip(
+        [col1, col2, col3, col4, col5],
+        recommended_movie_names,
+        recommended_movie_posters
+    ):
+        with col:
+            st.text(name)
+            st.image(poster)
